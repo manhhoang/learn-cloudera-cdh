44785a320bf4c9269696ea17c9c1a2b9e384b292
Manh Hoang <hamanhhoang1102@gmail.com>
2016-05-12 20:21:09 +0800

    update

--- src/main/java/com/jd/impala/ImpalaJdbcClient.java
+++ src/main/java/com/jd/impala/ImpalaJdbcClient.java
@@ -7,63 +7,61 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ImpalaJdbcClient {
-  // static String JDBC_DRIVER = "com.cloudera.impala.jdbc41.Driver";
-  static String JDBC_DRIVER = "org.apache.hive.jdbc.HiveDriver";
-  // private static final String CONNECTION_URL =
-  // "jdbc:impala://ec2-54-151-149-245.ap-southeast-1.compute.amazonaws.com:21050/gkadmin;principal=impala/ip-10-167-7-239.ap-southeast-1.compute.internal@EXAMPLE.COM";
+	// static String JDBC_DRIVER = "com.cloudera.impala.jdbc41.Driver";
+	static String JDBC_DRIVER = "org.apache.hive.jdbc.HiveDriver";
+	// private static final String CONNECTION_URL =
+	// "jdbc:impala://ec2-54-151-149-245.ap-southeast-1.compute.amazonaws.com:21050/gkadmin;principal=impala/ip-10-167-7-239.ap-southeast-1.compute.internal@EXAMPLE.COM";
 
-  private static final String CONNECTION_URL =
-      "jdbc:hive2://ec2-54-151-149-245.ap-southeast-1.compute.amazonaws.com:21050/gkadmin;principal=impala/ip-10-167-7-239.ap-southeast-1.compute.internal@EXAMPLE.COM";
+	private static final String CONNECTION_URL = "jdbc:hive2://ec2-54-151-149-245.ap-southeast-1.compute.amazonaws.com:21050/gkadmin;principal=impala/ip-10-167-7-239.ap-southeast-1.compute.internal@EXAMPLE.COM";
 
-  // private static final String CONNECTION_URL = "jdbc:impala://54.251.55.194:21050/gkadmin";
+	// private static final String CONNECTION_URL =
+	// "jdbc:impala://54.251.55.194:21050/gkadmin";
 
-  public static void main(String[] args) {
-    System.setProperty("java.security.auth.login.config", "gss-jaas.conf");
-    System.setProperty("java.security.krb5.conf", "krb5.conf");
-    System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
-    System.setProperty("sun.security.krb5.debug", "true");
+	public static void main(String[] args) {
+		System.setProperty("java.security.auth.login.config", "gss-jaas.conf");
+		System.setProperty("java.security.krb5.conf", "krb5.conf");
+		System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
+		System.setProperty("sun.security.krb5.debug", "true");
 
-    Connection con = null;
-    Statement stmt = null;
-    ResultSet rs = null;
-    String query = "select * from emstestevents order by 'timestamp' ASC limit 100";
-    // String query = "SHOW TABLES";
-    try {
-      Class.forName(JDBC_DRIVER);
-      con = DriverManager.getConnection(CONNECTION_URL);
-      stmt = con.createStatement();
-      rs = stmt.executeQuery(query);
-      while (rs.next()) {
-        System.out.println(String.valueOf(rs.getInt(1)) + "\t" + rs.getString(2) + "\t"
-            + rs.getString(3) + "\t" + rs.getString(4) + "\t" + rs.getString(5) + "\t"
-            + rs.getString(6) + "\t" + rs.getString(7) + "\t" + rs.getString(8) + "\t"
-            + rs.getString(9));
-        // System.out.println(String.valueOf(rs.getString(1)));
-      }
-    } catch (SQLException se) {
-      se.printStackTrace();
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      // Perform clean up
-      try {
-        if (rs != null) {
-          rs.close();
-        }
-      } catch (SQLException se1) {
-      }
-      try {
-        if (stmt != null) {
-          stmt.close();
-        }
-      } catch (SQLException se2) {
-      }
-      try {
-        if (con != null) {
-          con.close();
-        }
-      } catch (SQLException se4) {
-      }
-    }
-  }
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String query = "select * from emstestevents order by 'timestamp' ASC limit 100";
+		// String query = "SHOW TABLES";
+		try {
+			Class.forName(JDBC_DRIVER);
+			con = DriverManager.getConnection(CONNECTION_URL);
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				System.out.println(String.valueOf(rs.getInt(1)) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t"
+						+ rs.getString(4) + "\t" + rs.getString(5) + "\t" + rs.getString(6));
+				// System.out.println(String.valueOf(rs.getString(1)));
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// Perform clean up
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (SQLException se1) {
+			}
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+			} catch (SQLException se2) {
+			}
+			try {
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException se4) {
+			}
+		}
+	}
 }
