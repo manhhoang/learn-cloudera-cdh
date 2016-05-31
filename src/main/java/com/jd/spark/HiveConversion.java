2443ee77845b5c9b84f92b460ed4f6b1f75a0c4d
Mike Ha <hamanhhoang1102@gmail.com>
2016-05-31 14:38:12 +0800

    change have context

--- src/main/java/com/jd/spark/HiveConversion.java
+++ src/main/java/com/jd/spark/HiveConversion.java
@@ -1,61 +1,68 @@
 package com.jd.spark;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.hive.HiveContext;
 
 public class HiveConversion {
-  private static String driverName = "org.apache.hive.jdbc.HiveDriver";
 
   public static void main(String[] args) throws SQLException, IOException, InterruptedException {
-    try {
-      Class.forName(driverName);
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-    // System.setProperty("java.security.auth.login.config", "gss-jaas.conf");
-    System.setProperty("java.security.krb5.conf", "krb5.conf");
-    System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
-    System.setProperty("sun.security.krb5.debug", "false");
-
-    org.apache.hadoop.conf.Configuration conf = new org.apache.hadoop.conf.Configuration();
-    conf.set("hadoop.security.authentication", "kerberos");
-    UserGroupInformation.setConfiguration(conf);
-    UserGroupInformation.loginUserFromKeytab("hive@EXAMPLE.COM", "hive.keytab");
-    Connection con =
-        DriverManager
-            .getConnection("jdbc:hive2://ec2-54-151-149-245.ap-southeast-1.compute.amazonaws.com:10000/gkadmin;principal=hive/ip-10-167-7-239.ap-southeast-1.compute.internal@EXAMPLE.COM");
-    Statement stmt = con.createStatement();
-    String sql = "select * from node";
-    ResultSet res = stmt.executeQuery(sql);
-    List<Node> nodes = new ArrayList<Node>();
-    while (res.next()) {
-      System.out.println(String.valueOf(res.getLong(1)) + "\t" + res.getString(2) + "\t"
-          + res.getString(3) + "\t" + res.getString(4) + "\t" + res.getString(5));
-      nodes.add(new Node(1, "Test"));
-    }
-
     SparkConf sparkConf = new SparkConf().setAppName("HiveConversion");
     JavaSparkContext ctx = new JavaSparkContext(sparkConf);
-    JavaRDD<Node> nodeRDD = ctx.parallelize(nodes);
-
     HiveContext hiveContext = new HiveContext(ctx.sc());
+    Row[] results = hiveContext.sql("SELECT * FROM gkadmin.node_raw").collect();
+    List<Node> nodes = new ArrayList<Node>();
+    for (Row row : results) {
+      Node node = new Node();
+      node.setId(Long.parseLong(String.valueOf(row.getAs("id"))));
+      node.setName(String.valueOf(row.getAs("name")));
+      node.setDisplayName(String.valueOf(row.getAs("displayname")));
+      node.setResource(String.valueOf(row.getAs("type")));
+      node.setGroupId(Integer.parseInt(String.valueOf(row.getAs("groupid"))));
+      String genericcolumn = String.valueOf(row.getAs("genericcolumn"));
+      String value = String.valueOf(row.getAs("value"));
+      if (genericcolumn.equals("floor_area") && !value.trim().equals("")) {
+        node.setFloor_area(Float.parseFloat(value));
+      }
+      if (genericcolumn.equals("occupant")) {
+        node.setOccupant(Integer.parseInt(value));
+      }
+      if (genericcolumn.equals("owner_name")) {
+        node.setOwner_name(value);
+      }
+      if (genericcolumn.equals("built_date")) {
+        node.setBuilt_date(Long.parseLong(value));
+      }
+      if (genericcolumn.equals("green_mark")) {
+        node.setGreen_mark(value);
+      }
+      if (genericcolumn.equals("node_group")) {
+        node.setNode_group(value);
+      }
+      if (genericcolumn.equals("longitude")) {
+        node.setLongitude(value);
+      }
+      if (genericcolumn.equals("latitude")) {
+        node.setLatitude(value);
+      }
+      if (genericcolumn.equals("locationType")) {
+        node.setLocationType(value);
+      }
+      nodes.add(node);
+    }
+
+    JavaRDD<Node> nodeRDD = ctx.parallelize(nodes);
     DataFrame hiveSchemaDF = hiveContext.createDataFrame(nodeRDD, Node.class);
-    String satTable = "node_convertion";
+    String satTable = "gkadmin.node";
     hiveSchemaDF.saveAsTable(satTable, SaveMode.Append);
     ctx.stop();
   }
--- src/main/java/com/jd/spark/Node.java
+++ src/main/java/com/jd/spark/Node.java
@@ -7,11 +7,20 @@ public class Node implements Serializable {
 
   private long id;
   private String name;
-
-  public Node(long id, String name) {
-    this.id = id;
-    this.name = name;
-  }
+  private String displayName;
+  private String resource;
+  private String loadType;
+  private int groupId;
+  private float floor_area;
+  private int occupant;
+  private String owner_name;
+  private String longitude;
+  private String latitude;
+  private String node_group;
+  private String widget;
+  private long built_date;
+  private String green_mark;
+  private String locationType;
 
   public long getId() {
     return id;
@@ -27,5 +36,117 @@ public class Node implements Serializable {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  public void setDisplayName(String displayName) {
+    this.displayName = displayName;
+  }
+
+  public String getResource() {
+    return resource;
+  }
+
+  public void setResource(String resource) {
+    this.resource = resource;
+  }
+
+  public String getLoadType() {
+    return loadType;
+  }
+
+  public void setLoadType(String loadType) {
+    this.loadType = loadType;
+  }
+
+  public int getGroupId() {
+    return groupId;
+  }
+
+  public void setGroupId(int groupId) {
+    this.groupId = groupId;
+  }
+
+  public float getFloor_area() {
+    return floor_area;
+  }
+
+  public void setFloor_area(float floor_area) {
+    this.floor_area = floor_area;
+  }
+
+  public int getOccupant() {
+    return occupant;
+  }
+
+  public void setOccupant(int occupant) {
+    this.occupant = occupant;
+  }
+
+  public String getOwner_name() {
+    return owner_name;
+  }
+
+  public void setOwner_name(String owner_name) {
+    this.owner_name = owner_name;
+  }
+
+  public String getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(String longitude) {
+    this.longitude = longitude;
+  }
+
+  public String getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(String latitude) {
+    this.latitude = latitude;
+  }
+
+  public String getNode_group() {
+    return node_group;
+  }
+
+  public void setNode_group(String node_group) {
+    this.node_group = node_group;
+  }
+
+  public String getWidget() {
+    return widget;
+  }
+
+  public void setWidget(String widget) {
+    this.widget = widget;
+  }
+
+  public long getBuilt_date() {
+    return built_date;
+  }
+
+  public void setBuilt_date(long built_date) {
+    this.built_date = built_date;
+  }
+
+  public String getGreen_mark() {
+    return green_mark;
+  }
+
+  public void setGreen_mark(String green_mark) {
+    this.green_mark = green_mark;
+  }
+
+  public String getLocationType() {
+    return locationType;
+  }
+
+  public void setLocationType(String locationType) {
+    this.locationType = locationType;
   }
 }
